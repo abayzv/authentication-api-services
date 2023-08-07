520a0b1ef30135a82481c47ff7caf83972909960
DESKTOP-9VU9I7S\Rai <ajipunk008@gmail.com>
2023-08-07 13:42:31 +0700

    init

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,10 +7,6 @@ import auth from "./auth/auth.routes";
 import users from "./users/users.routes";
 import role from "./role/role.routes";
 import permission from "./permission/permission.routes";
-import whatsapp from "./whatsapp/whatsapp.routes";
-import contact from "./contact/contact.routes";
-import contactGroup from "./contactGroup/contactGroup.routes";
-import broadcast from "./broadcast/broadcast.routes";
 
 const router = express.Router();
 
@@ -18,10 +14,6 @@ router.use("/auth", auth);
 router.use("/users", users);
 router.use("/roles", role);
 router.use("/permissions", permission);
-router.use("/wa-gateway", whatsapp);
-router.use("/contacts", contact);
-router.use("/contact-groups", contactGroup);
-router.use("/message-log", broadcast);
 
 // Swagger setup
 
